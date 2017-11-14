4bd3d282d7e4079128fa3598f1c6bdccc4d91f74
jiayaoyao <287824764@qq.com>
2017-11-14 22:52:48 +0800

    first

---
+++ src/components/question/list.vue
@@ -0,0 +1,373 @@
+<template>
+  <el-row class="warp">
+    <el-col :span="24" class="warp-breadcrum">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }"><b>首页</b></el-breadcrumb-item>
+        <el-breadcrumb-item>学生管理</el-breadcrumb-item>
+      </el-breadcrumb>
+    </el-col>
+
+    <el-col :span="24" class="warp-main">
+      <!--工具条-->
+      <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+        <el-form :inline="true" :model="filters">
+          <el-form-item>
+            <el-input v-model="filters.name" placeholder="姓名"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" v-on:click="getDatas">查询</el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="showAddDialog">新增</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+
+      <!--列表-->
+      <el-table :data="books" highlight-current-row v-loading="listLoading" @selection-change="selsChange"
+                style="width: 100%;">
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column prop="id" label="编号" sortable></el-table-column>
+        <el-table-column type="expand">
+          <template scope="props">
+            <el-form>
+              <el-form-item label="标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一标签一"></br>
+                <el-checkbox-group >
+                  <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox></br>
+                  <el-checkbox label="地推活动" name="type"></el-checkbox></br>
+                  <el-checkbox label="线下主题活动" name="type"></el-checkbox></br>
+                  <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+                </el-checkbox-group>
+              </el-form-item>
+              <el-form-item label="[图书简介]">
+                <span>{{ props.row.description }}</span>
+              </el-form-item>
+            </el-form>
+          </template>
+        </el-table-column>
+        <el-table-column prop="type" label="类型" sortable></el-table-column>
+        <el-table-column prop="point" label="考点" sortable></el-table-column>
+        <el-table-column prop="lv" label="难易程度" sortable></el-table-column>
+        <el-table-column label="操作" width="150">
+          <template scope="scope">
+            <el-button size="small" @click="showEditDialog(scope.$index,scope.row)">编辑</el-button>
+            <el-button type="danger" @click="delData(scope.$index,scope.row)" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!--工具条-->
+      <el-col :span="24" class="toolbar">
+        <el-button type="danger" @click="batchDeleteData" :disabled="this.sels.length===0">批量删除</el-button>
+        <el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="10" :total="total" style="float:right;">
+        </el-pagination>
+      </el-col>
+
+      <el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
+        <el-form :model="editForm" label-width="100px" :rules="editFormRules" ref="editForm">
+          <el-form-item label="姓名" prop="name">
+            <el-input v-model="editForm.name" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="编号" prop="id">
+            <el-input v-model="editForm.id" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="班级" prop="clas">
+            <el-input v-model="editForm.clas" auto-complete="off"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click.native="editFormVisible = false">取消</el-button>
+          <el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
+        </div>
+      </el-dialog>
+
+      <!--新增界面-->
+      <el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
+        <el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
+          <el-form-item label="题目" prop="title">
+            <el-input v-model="addForm.title" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="类型" prop="type">
+            <el-select v-model="addForm.type" placeholder="请选择">
+              <el-option
+                v-for="item in typeOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="知识点" prop="point">
+            <el-select v-model="addForm.point" placeholder="请选择">
+              <el-option
+                v-for="item in lvOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="难易程度" prop="lv">
+            <el-select v-model="addForm.lv" placeholder="请选择">
+              <el-option
+                v-for="item in lvOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="选择题" prop="title" v-show="addForm.type === '1'">
+            <el-checkbox v-model="addForm.a_checked">A</el-checkbox>
+            <el-input v-model="addForm.a_choice" auto-complete="off"></el-input>
+            <el-checkbox v-model="addForm.b_checked">B</el-checkbox>
+            <el-input v-model="addForm.b_choice" auto-complete="off"></el-input>
+            <el-checkbox v-model="addForm.c_checked">C</el-checkbox>
+            <el-input v-model="addForm.c_choice" auto-complete="off"></el-input>
+            <el-checkbox v-model="addForm.d_checked">D</el-checkbox>
+            <el-input v-model="addForm.d_choice" auto-complete="off"></el-input>
+            <el-checkbox v-model="addForm.e_checked">E</el-checkbox>
+            <el-input v-model="addForm.e_choice" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item inline label="简答题" prop="title" v-show="addForm.type === '2'">
+            <el-input
+              type="textarea"
+              :autosize="{ minRows: 2, maxRows: 4}"
+              placeholder="请输入简答题答案"
+              v-model="addForm.answer">
+            </el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click.native="addFormVisible = false">取消</el-button>
+          <el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+        </div>
+      </el-dialog>
+
+    </el-col>
+  </el-row>
+</template>
+<script>
+  import util from '../../common/util'
+  import {reqGetBookListPage, reqDeleteBook, reqEditBook, reqbatchDeleteData, reqAddBook} from '../../api/api';
+
+  export default{
+    data(){
+      return {
+        filters: {
+          name: ''
+        },
+        books: [],
+        total: 0,
+        page: 1,
+        listLoading: false,
+        sels: [], //列表选中列
+
+        //编辑相关数据
+        editFormVisible: false,//编辑界面是否显示
+        editLoading: false,
+        editFormRules: {
+          name: [
+            {required: true, message: '请输入书名', trigger: 'blur'}
+          ],
+          id: [
+            {required: true, message: '请输入编号', trigger: 'blur'}
+          ],
+          clas: [
+            {required: true, message: '请输入班级', trigger: 'blur'}
+          ]
+        },
+        editForm: {
+          id: '',
+          title: '',
+          type: '',
+          lv: ''
+        },
+
+        //新增相关数据
+        addFormVisible: false,//新增界面是否显示
+        addLoading: false,
+        addFormRules: {
+          name: [
+            {required: true, message: '请输入书名', trigger: 'blur'}
+          ],
+          id: [
+            {required: true, message: '请输入编号', trigger: 'blur'}
+          ],
+          clas: [
+            {required: true, message: '请输入班级', trigger: 'blur'}
+          ]
+        },
+        addForm: {
+          id: '',
+          title: '',
+          type: '',
+          lv: '',
+          a_choice: '这是一个选项',
+          b_choice: '这是一个选项',
+          c_choice: '这是一个选项',
+          d_choice: '这是一个选项',
+          e_choice: '这是一个选项',
+
+          a_checked: false,
+          b_checked: false,
+          c_checked: false,
+          d_checked: false,
+          e_checked: false,
+
+          answer:''
+        },
+        typeOptions: [{
+          value: '1',
+          label: '选择题'
+        }, {
+          value: '2',
+          label: '简答题'
+        }],
+        lvOptions: [{
+          value: '1',
+          label: '易'
+        }, {
+          value: '2',
+          label: '中'
+        }, {
+          value: '3',
+          label: '难'
+        }]
+      }
+    },
+    methods: {
+      handleCurrentChange(val) {
+        this.page = val;
+        this.getDatas();
+      },
+      //获取用户列表
+      getDatas() {
+        let para = {
+          page: this.page,
+          name: this.filters.name
+        };
+        this.listLoading = true;
+        //NProgress.start();
+        reqGetBookListPage(para).then((res) => {
+          this.total = res.data.total;
+          this.books = res.data.books;
+          this.listLoading = false;
+          //NProgress.done();
+        })
+      },
+      selsChange: function (sels) {
+        this.sels = sels;
+      },
+      //删除
+      delData: function (index, row) {
+        this.$confirm('确认删除该记录吗?', '提示', {type: 'warning'}).then(() => {
+          this.listLoading = true;
+          //NProgress.start();
+          let para = {id: row.id};
+          reqDeleteBook(para).then((res) => {
+            this.listLoading = false;
+            //NProgress.done();
+            this.$message({
+              message: '删除成功',
+              type: 'success'
+            });
+            this.getDatas();
+          });
+        }).catch(() => {
+        });
+      },
+      //显示编辑界面
+      showEditDialog: function (index, row) {
+        this.editFormVisible = true;
+        this.editForm = Object.assign({}, row);
+      },
+      //编辑
+      editSubmit: function () {
+        this.$refs.editForm.validate((valid) => {
+          if (valid) {
+            this.$confirm('确认提交吗？', '提示', {}).then(() => {
+              this.editLoading = true;
+              //NProgress.start();
+              let para = Object.assign({}, this.editForm);
+              para.publishAt = (!para.publishAt || para.publishAt == '') ? '' : util.formatDate.format(new Date(para.publishAt), 'yyyy-MM-dd');
+              reqEditBook(para).then((res) => {
+                this.editLoading = false;
+                //NProgress.done();
+                this.$message({
+                  message: '提交成功',
+                  type: 'success'
+                });
+                this.$refs['editForm'].resetFields();
+                this.editFormVisible = false;
+                this.getDatas();
+              });
+            });
+          }
+        });
+      },
+      showAddDialog: function () {
+        this.addFormVisible = true;
+        this.addForm = {
+          name: '',
+          author: '',
+          publishAt: '',
+          description: ''
+        };
+      },
+      //新增
+      addSubmit: function () {
+        this.$refs.addForm.validate((valid) => {
+          if (valid) {
+            this.addLoading = true;
+            //NProgress.start();
+            let para = Object.assign({}, this.addForm);
+            para.publishAt = (!para.publishAt || para.publishAt == '') ? '' : util.formatDate.format(new Date(para.publishAt), 'yyyy-MM-dd');
+            reqAddBook(para).then((res) => {
+              this.addLoading = false;
+              //NProgress.done();
+              this.$message({
+                message: '提交成功',
+                type: 'success'
+              });
+              this.$refs['addForm'].resetFields();
+              this.addFormVisible = false;
+              this.getDatas();
+            });
+          }
+        });
+      },
+      //批量删除
+      batchDeleteData: function () {
+        var ids = this.sels.map(item => item.id).toString();
+        this.$confirm('确认删除选中记录吗？', '提示', {
+          type: 'warning'
+        }).then(() => {
+          this.listLoading = true;
+          //NProgress.start();
+          let para = {ids: ids};
+          reqbatchDeleteData(para).then((res) => {
+            this.listLoading = false;
+            //NProgress.done();
+            this.$message({
+              message: '删除成功',
+              type: 'success'
+            });
+            this.getDatas();
+          });
+        }).catch(() => {
+
+        });
+      }
+    },
+    mounted() {
+      this.getDatas();
+    }
+  }
+</script>
+
+<style>
+  .demo-table-expand label {
+    font-weight: bold;
+  }
+</style>
